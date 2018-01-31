6ea844c6f2e4b5860a3669cee1e6a6662bb3a112
Angular CLI <angular-cli@angular.io>
2018-01-31 15:07:34 +0000

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ src/assets/build/app/zsxtkdjv.js
@@ -0,0 +1,2 @@
+/*! Built with http://stenciljs.com */
+const{h,Context}=window.App;class AppProfile{render(){if(this.match&&this.match.params.name)return h("div",null,h("p",null,"Hello! My name is ",this.match.params.name,". My name was passed in through a route param!"))}static get is(){return"app-profile"}static get properties(){return{match:{type:"Any"}}}static get style(){return"app-profile div{padding:10px}"}}export{AppProfile};
